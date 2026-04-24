85767ae841dabd93086d00453275608ac6ee1cc8
indahamalia045 <indahamalia1605@gmail.com>
2026-04-24 11:22:31 +0700

    update terbaru

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const CONFIG = {
     }
   },
   mapsLink: "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d249.28844044258796!2d99.29786488441009!3d1.4071944663445939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1776677636164!5m2!1sid!2sid",
-  musicUrl: "/music/onang.mp3",
+  musicUrl: "/music/onango.mp3",
   photos: [
     "/images/1.jpeg",
     "/images/2.jpeg",
