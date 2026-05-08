deacc9018565a4c77f68b075c6b71e9df38365ab
indahamalia045 <indahamalia1605@gmail.com>
2026-05-08 11:06:47 +0700

    update terbaru

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ const CONFIG = {
     "/images/6.jpeg",
     "/images/7.jpeg",
     "/images/8.jpeg",
-    "/images/10.jpeg",
+    "/images/9.jpeg",
     "/images/11.jpeg",
     "/images/12.jpeg",
     "/images/13.jpeg",
