9ad08e97161fdaeb53d30c92f1746ce32e829f18
indahamalia045 <indahamalia1605@gmail.com>
2026-05-08 11:09:11 +0700

    update terbaru

--- src/App.js
+++ src/App.js
@@ -78,14 +78,11 @@ const CONFIG = {
     "/images/2.jpeg",
     "/images/3.jpeg",
     "/images/4.jpeg",
-    "/images/5.jpeg",
-    "/images/6.jpeg",
-    "/images/7.jpeg",
-    "/images/8.jpeg",
+    
     "/images/9.jpeg",
     "/images/11.jpeg",
     "/images/12.jpeg",
-    "/images/13.jpeg",
+    
     "/images/14.jpeg"
   ],
   coverPhoto: "/images/1.jpeg",
